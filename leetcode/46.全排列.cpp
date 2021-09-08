ff2f54c8ecc57b67d9a2e8800f637433fb3856ef
sunyu <sunyu@indemind.cn>
2021-09-08 14:32:35 +0800

    修改完成

--- leetcode/46.全排列.cpp
+++ leetcode/46.全排列.cpp
@@ -7,26 +7,29 @@
 // @lc code=start
 class Solution {
 public:
-    void backtrack(vector<vector<int>>& res, vector<int>& output, int first, int len){
-        // 所有数都填完了
-        if (first == len - 1) {
-            res.emplace_back(output);
-            return;
-        }
-        for (int i = first; i < len; ++i) {
-            // 动态维护数组
-            swap(output[i], output[first]);
-            // 继续递归填下一个数
-            backtrack(res, output, first + 1, len);
-            // 撤销操作
-            swap(output[i], output[first]);
-        }
-    }
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int> > res;
-        backtrack(res, nums, 0, (int)nums.size());
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        vector<vector<int>> res;
+        backtrace( nums, res, 0 );
         return res;
     }
+
+    void backtrace( vector<int>&  nums, vector<vector<int>>& res, int depth)
+    {
+        if ( depth == nums.size() -1 )      // // 所有数都填完了
+        {
+            res.emplace_back(nums);
+            return;
+        }
+
+        for ( int i = depth; i < nums.size(); i++ )
+        {
+            swap( nums[i], nums[depth] );            // 修改当前节点状态
+            backtrace( nums, res, depth + 1 );      // 继续递归填写下一个数
+            swap( nums[i], nums[depth] );           // 撤销操作，回改当前节点状态
+        }
+    }
+    
 };
 // @lc code=end
 
