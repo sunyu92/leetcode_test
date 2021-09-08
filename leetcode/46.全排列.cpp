eb99214d2f4d91f34f62d1f00c7f75087cf1f39e
sunyu <sunyu@indemind.cn>
2021-09-08 14:28:00 +0800

    46 全排列完成

--- leetcode/46.全排列.cpp
+++ leetcode/46.全排列.cpp
@@ -7,9 +7,29 @@
 // @lc code=start
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        vector<vector<int>> res;
+        backtrace( nums, res, 0 );
+        return res;
     }
+
+    void backtrace( vector<int>&  nums, vector<vector<int>>& res, int depth)
+    {
+        if ( depth == nums.size() -1 )      // // 所有数都填完了
+        {
+            res.emplace_back(nums);
+            return;
+        }
+
+        for ( int i = depth; i < nums.size(); i++ )
+        {
+            swap( nums[i], nums[depth] );            // 修改当前节点状态
+            backtrace( nums, res, depth + 1 );      // 继续递归填写下一个数
+            swap( nums[i], nums[depth] );           // 撤销操作，回改当前节点状态
+        }
+    }
+    
 };
 // @lc code=end
 
