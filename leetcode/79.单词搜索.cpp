c21c7419322b98dea49fabcf2d0957fc5cb94421
sunyu <sunyu@indemind.cn>
2021-09-08 18:31:11 +0800

    完成

--- leetcode/79.单词搜索.cpp
+++ leetcode/79.单词搜索.cpp
@@ -7,8 +7,8 @@
 // @lc code=start
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word)
-     {
+    bool exist(vector<vector<char>>& board, string word) 
+    {
         if (board.empty())
         {
             return false;
@@ -17,21 +17,20 @@ public:
         int m = board.size(), n = board[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
         bool find = false;
-        for (int i = 0; i < m; ++i)
-         {
-            for (int j = 0; j < n; ++j)
-             {
-                backtracking(i, j, board, word, find, visited, 0);
-            }
-    }
-    return find;
-}
 
-    // 辅函数
-    void backtracking(int i, int j, vector<vector<char>>& board, string& word, bool
-    & find, vector<vector<bool>>& visited, int wordIndex)
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                backtrack(i, j, board, word, find, visited, 0);
+            }
+        }
+        return find;
+    }
+
+    void backtrack(int i, int j, vector<vector<char>>& board, string& word, bool& find, vector<vector<bool>>& visited, int wordIndex)
     {
-        // 检测是否出边界
+        // 检查是否出边界
         if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size())
          {
             return;
@@ -43,21 +42,22 @@ public:
             return;
         }
 
-        // 成功搜索到单词末尾，返回true
-        if (wordIndex == word.size() - 1) 
+        // 找到并返回 true 结果， 成功搜索到单词末尾，返回true
+        if (wordIndex == word.size() - 1)
         {
             find = true;
             return;
         }
 
-        visited[i][j] = true; // 修改当前节点状态
-        // 递归子节点
-        backtracking(i + 1, j, board, word, find, visited, wordIndex + 1);
-        backtracking(i - 1, j, board, word, find, visited, wordIndex + 1);
-        backtracking(i, j + 1, board, word, find, visited, wordIndex + 1);
-        backtracking(i, j - 1, board, word, find, visited, wordIndex + 1);
-        visited[i][j] = false; // 回改当前节点状态
-        }
+        // 标记
+        visited[i][j] = true;   // 修改当前节点状态
+        // // 递归子节点
+        backtrack(i - 1, j, board, word, find, visited, wordIndex + 1);
+        backtrack(i + 1, j, board, word, find, visited, wordIndex + 1);
+        backtrack(i, j - 1, board, word, find, visited, wordIndex + 1);
+        backtrack(i, j + 1, board, word, find, visited, wordIndex + 1);
+        visited[i][j] = false;      // 回改当前节点状态
+    }
 };
 // @lc code=end
 
