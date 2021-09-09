e3b81885fea1c1513e0076a3e46903e7a62de2a7
sunyu <sunyu@indemind.cn>
2021-09-09 17:53:56 +0800

    查表法

--- leetcode/509.斐波那契数.cpp
+++ leetcode/509.斐波那契数.cpp
@@ -7,8 +7,31 @@
 // @lc code=start
 class Solution {
 public:
-    int fib(int n) {
+    int fib(int N) {
+        if (N < 1)
+        {
+            return 0;
+        }
 
+        vector<int> memo(N + 1, 0);
+        
+        return helper(memo, N);
+    }
+
+    int helper(vector<int>& memo, int n)
+    {
+        if (n == 1 || n == 2)
+        {
+            return 1;
+        }
+
+        if (memo[n] != 0)
+        {
+            return memo[n];
+        }
+
+        memo[n] = helper(memo, n - 1) + helper(memo, n - 2);
+        return memo[n];
     }
 };
 // @lc code=end
