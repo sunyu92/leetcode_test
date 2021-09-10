f6c814c11f653fb5ee16c74e19668979a79597f5
sunyu <sunyu@indemind.cn>
2021-09-10 11:03:48 +0800

    子问题

--- leetcode/322.零钱兑换.cpp
+++ leetcode/322.零钱兑换.cpp
@@ -8,14 +8,18 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        // 数组大小为 amount + 1,初始值也为 amount + 1
         vector<int> dp(amount + 1, amount + 1);
-
+        // base case
         dp[0] = 0;
 
         for (int i= 0; i < dp.size(); i++)
         {
-            for (auto coin : coins)
+            // 内层 for 在求所有子问题 + 1 的最小值
+            for (const int & coin : coins)
             {
+                {
+                // 子问题无解,跳过
                 if (i - coin < 0)
                 {
                     continue;
