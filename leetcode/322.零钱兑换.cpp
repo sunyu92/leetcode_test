cee529f6a79522171fdb67d033d769702ef6de61
sunyu <sunyu@indemind.cn>
2021-09-10 10:21:03 +0800

    上传

--- leetcode/322.零钱兑换.cpp
+++ leetcode/322.零钱兑换.cpp
@@ -8,7 +8,23 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1);
 
+        dp[0] = 0;
+
+        for (int i= 0; i < dp.size(); i++)
+        {
+            for (auto coin : coins)
+            {
+                if (i - coin < 0)
+                {
+                    continue;
+                }
+
+                dp[i] = min(dp[i], 1 + dp[i - coin]);
+            }
+        }
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 };
 // @lc code=end
