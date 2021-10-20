72d10927ba3250fa16d895ad35c0763eceaad7ac
sunyu <sunyu@indemind.cn>
2021-10-20 15:59:59 +0800

    滑动窗口

--- leetcode/104.二叉树的最大深度.cpp
+++ leetcode/104.二叉树的最大深度.cpp
@@ -19,7 +19,7 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-
+        return root ? 1 + max(maxDepth(root->left), maxDepth(root->right)) : 0;
     }
 };
 // @lc code=end
---
+++ leetcode/959.由斜杠划分区域.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=959 lang=cpp
+ *
+ * [959] 由斜杠划分区域
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int regionsBySlashes(vector<string>& grid) {
+
+    }
+};
+// @lc code=end
+
