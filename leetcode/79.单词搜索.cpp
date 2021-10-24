7807b1fde14711b73fe58930b19cd08e398d4346
Yu Sun <31779522+sunyu92@users.noreply.github.com>
2021-10-24 19:13:11 +0800

    main

--- leetcode/79.单词搜索.cpp
+++ leetcode/79.单词搜索.cpp
@@ -24,18 +24,21 @@ public:
     }
 
     bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
+        // 满足结束条件
         if (!word.size())
             return true;
+        // 边界条件，判断是否有效
         if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
             return false;
+        
+        // 做选择
         char c = board[i][j];
         board[i][j] = '*';
         string s = word.substr(1);
         bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
-        board[i][j] = c;
-        return ret;
+        board[i][j] = c; // 恢复现场
+        return ret;	// 返回结果
     }
-
 };
 // @lc code=end
 
